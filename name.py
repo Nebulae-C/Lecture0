76dfa1b469ed25dec8a5f1d59c67737b38b77170
Nebulae-C <harrygrady08@gmail.com>
2026-01-28 16:41:44 +1100

    added some test python files for future integration.

---
+++ name.py
@@ -0,0 +1,24 @@
+name = input()
+print(f"hello, {name}")
+
+i = 28
+f = 2.8
+b = True
+n = None
+x = 28
+
+print(f"i is {i}")
+print(f"f is {f}")
+print(f"b is {b}")
+print(f"n is {n}")
+
+if x > 0:
+    print("X is positive")
+elif x < 0:
+    print("X is negative")
+else:
+    print("X is zero")
+    
+name = "Alice"
+coordinates = (10.0, 20.0)
+names = ["Alice", "Bob", "Charlie"]
